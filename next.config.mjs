9249fcbcdcb334cb020fbb5e0c20f0a36d78a5a8
NguyenNhuDuyHung <duyhung03112004@gmail.com>
2025-05-22 00:01:19 +0700

    Create Db and Fetch Data

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "images.pexels.com" }],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3001/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
